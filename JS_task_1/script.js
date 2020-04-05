bfbd0d5b716e0e3fdd7798a610d1e782dae3a986
ZICKO8 <61982824+ZICKO8@users.noreply.github.com>
2020-04-05 12:36:54 +0100

    first website upload

--- JS_task_1/script.js
+++
@@ -1,9 +0,0 @@
-let name = "ajayiayodele";
-console.log("my name is: " + name);
-
-const courses = ["html", "css", "javascript"];
-console.log(courses);
-
-for (let i = 0; i <= 200; i++) {
-    if (i % 2 !== 0) console.log(i);
-    }
